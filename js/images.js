3620d4f615ab490a161d1a4a783d0a3ce064119e
Zack Claar <claar.zack@gmail.com>
2019-08-22 23:07:50 -0600

    Background Image Repo and Drawable in their own files, imported these at top of app.js

---
+++ js/images.js
@@ -0,0 +1,10 @@
+export default images = {
+    imageRepo = new function() {
+        // Defining the images
+        this.empty = null;
+        this.background = new Image();
+        
+        // Set image srource files
+        this.background.src = "./img/road.png";
+    }
+}
