71180c655eb8dd247f3e85ac4e6a712fa8ecb08b
Zack Claar <claar.zack@gmail.com>
2019-11-06 18:28:14 -0700

    Add sources for ship and bullets. Add function to ensure images load before starting game. Add height and width values in drawable class as well as abstract move function.

--- js/app.js
+++ js/app.js
@@ -5,9 +5,30 @@ var imageRepo = new function() {
     // Define images below
     this.empty = null;
     this.background = new Image();
-
+    this.spaceship = new Image();
+    this.bullet = new Image();
+    // Making sure that all the images have loaded before starting the game
+    var numImages = 3;
+    var numLoaded = 0;
+    function imageLoaded() {
+        numLoaded ++;
+        if (numLoaded === numImages) {
+            window.init();
+        }
+    }
+    this.background.onload = function() {
+        imageLoaded();
+    }
+    this.spaceship.onload = function() {
+        imageLoaded();
+    }
+    this.bullet.onload = function() {
+        imageLoaded();
+    }
     // Set the images source
     this.background.src = "imgs/bg.png";
+    this.spaceship.src = "imgs/space-ship.png";
+    this.bullet.src = "imgs/bullet.png";
 };
 
 
@@ -16,19 +37,22 @@ var imageRepo = new function() {
 // Sets default balues that all child object will inherit,
 // as well as the default functions they will inherit.
 function Drawable() {
-    this.init = function(x, y) {
-        // Setting default x and y values
+    this.init = function(x, y, height, width) {
+        // Setting default x, y, height, and width values
         this.x = x;
         this.y = y;
+        this.height = height;
+        this.width = width;
     }
     this.speed = 0;
     this.canvasWidth = 0;
     this.canvasHeight = 0; 
-
-    // Defining a abstract function that will be implemented 
+    // Defining abstract functions that will be implemented 
     // in children of Drawable 
     this.draw = function() {
-    }
+    };
+    this.move = function() {
+    };
 };
 
 
@@ -53,6 +77,16 @@ function Background() {
 Background.prototype = new Drawable();
 
 
+// Ensuring the background music has loaded before starting the game
+function checkReadyState() {
+    if (game.backgroundAudio.readyState === 4) {
+        console.log('CHECK READY BRUH.');
+        window.clearInterval(game.checkAudio);
+
+    }
+}
+
+
 // Animation loop here! Calls 
 function animate() {
     requestAnimFrame( animate );
@@ -66,6 +100,7 @@ function Game() {
         // all of the game objects. Initially init function will return true
         // if browser supports HTML5 canvas, and false if it does not. 
         this.bgCanvas = document.getElementById('background-canvas');
+        this.menuCanvas = document.getElementById('menu-canvas');
         // Test to see whether or not browser supports HTML5 canvas elements
         // by attempting to get the context of the element on the HTML page
         if (this.bgCanvas.getContext) {
@@ -76,21 +111,49 @@ function Game() {
             Background.prototype.canvasWidth = this.bgCanvas.width;
             Background.prototype.canvasHeight = this.bgCanvas.height;
             // Initialize the background object!
-            this.background = new Background();
             // Setting initial draw location to cord 0, 0!
+            this.background = new Background();
             this.background.init(0, 0);
+            // Audio files
+            // this.bgCanvas.addEventListener("mouseover", function( event ) {
+                this.backgroundAudio = new Audio("audio/ip.mp3");
+                this.backgroundAudio.loop = true;
+                this.backgroundAudio.volume = 0.2;
+                this.backgroundAudio.load();
+                this.backgroundAudio.play();
+                
+            // })
+            // Checking and looping audio with setInterval
+            // this.checkAudio = window.setInterval(function(){
+            //     checkReadyState()
+            // }, 1000);
             return true;
         } else {
             return false;
         }
     }
-
     // Starting the animation loop!
     this.start = function() {
+        this.backgroundAudio.play();
         animate();
     }
-    
 };
+
+function startMusicXD() {
+    this.backgroundAudio = new Audio("audio/ip.mp3");
+    this.backgroundAudio.loop = true;
+    this.backgroundAudio.volume = 0.2;
+    this.backgroundAudio.load();
+    this.backgroundAudio.play();
+}
+
+// Creating a sound pool for sound effects
+// and game song.
+function SoundPool() {
+
+}
+
+
 
 // Initialize the game
 var game = new Game();
