76ac8979bb4bafbbf2bdeacc509ca8fd5d079b03
Zack Claar <claar.zack@gmail.com>
2019-11-07 18:27:39 -0700

    Add bullet class with default values and conditionals to check if bullet leaves canvas. Add Pool object which holds all bullets on canvas. Bullets alive (drawn on canvas) will be pushed to front of array and will br drawn until size of array is reached

--- js/app.js
+++ js/app.js
@@ -71,27 +71,118 @@ function Background() {
         if (this.y >= this.canvasHeight) {
             this.y = 0;
         }
-    }
+    };
 };
 // Setting background to inherit/be a child of the drawable class
 Background.prototype = new Drawable();
 
 
-// Ensuring the background music has loaded before starting the game
-function checkReadyState() {
-    if (game.backgroundAudio.readyState === 4) {
-        console.log('CHECK READY BRUH.');
-        window.clearInterval(game.checkAudio);
+// Creating the bullet object which fire from the ship
+// when the user presses spacebar and it is drawn on the main canvas
+function Bullet() {
+    // this will be true if the bullet is in use
+    this.alive = false;
+    // We are going to set the bullet values here
+    this.spawn = function(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.alive = true;
+    };
+    // DIRTY RECTANGLES!
+    // Checking to see if the bullet has gone off canvas
+    // if it has, we are going to clear it before it is redrawn
+    this.draw = function() {
+        this.context.clearRect(this.x, this.y, this.height, this.width);
+        this.y -= this.speed;
+        if (this.y <= 0 - this.height) {
+            return true;
+        } else {
+            this.context.drawImage(imageRepo.bullet, this.x, this.y);
+        }
+    };
+    // Resetting the bullet values
+    this.clear = function() {
+        this.x = 0;
+        this.y = 0;
+        this.speed = 0;
+        this.alive = false;
+    };
+};
+Bullet.prototype = new Drawable();
 
+
+// The POOL OBJECT! IMPORTANT!
+// this object holds bullet objects and manages them 
+// to prevent garbage collection and work like so:
+//     - When the pool is initialized, it will populate an array with bullet object
+//     - When the pool needs to create a bullet object for use, it will check to see if it
+//       is currently in use in the array or not in use. If it is in use, the pool is full and
+//       if it isn't in use, it will spawn the last item in the array and pops it from the end and 
+//       pushes it back into the front of the array. This makes the pool have free bullet objects
+//       at the back of the array and ones in use at the front
+//     - When the pool animates it objects, it checks to see if the object isn't in use; i.e. no need to draw
+//       and if it is, it will draw it! 
+//     - If the draw function returns true, the object is cleared and uses the array function splice() to remove
+//       item from the array and pushes it to the back
+//     - This entire pool make the creation and destruction of objects in the pool constant with gameplay
+function Pool(maxSize) {
+    var size = maxSize;
+    var pool = [];
+    // Populate the array with bullet objects
+    this.init = function() {
+        for (var i = 0; i < size; i++) {
+            // initialize the bulletz
+            var bullet = new Bullet();
+            bullet.init(0, 0, imageRepo.bullet.height, imageRepo.bullet.width);
+            pool[i] = bullet;
+        }
+    };
+    // Taking a free bullet [i] and pushing it to the front of the array
+    this.get = function(x, y, speed) {
+        if(!pool[size - 1].alive) {
+            pool[size - 1].spawn(x, y, speed);
+            pool.unshift(pool.pop());
+        }
+    };
+    // get two is used for the ship to shoot 2 bullets at once
+    this.getTwo = function(x1, y1, speed1, x2, y2, speed2) {
+        if(!pool[size - 1].alive && !pool[size - 2].alive) {
+            this.get(x1, y1, speed1);
+            this.get(x2, y2, speed2);
+        }
+    };
+    this.animate = function() {
+        for (var i = 0; i < size; i++) {
+            // only draw bullets until we find one in pool array that isn't 'alive'
+            if (pool[i].alive) {
+                if (pool[i].draw()) {
+                    pool[i].clear();
+                    pool.push((pool.space(i, 1))[0]);
+                }
+            } else {
+                break;
+            }
+        }
     }
 }
 
 
-// Animation loop here! Calls 
-function animate() {
-    requestAnimFrame( animate );
-    game.background.draw();
-}
+// Ensuring the background music has loaded before starting the game
+// function checkReadyState() {
+//     if (game.backgroundAudio.readyState === 4) {
+//         console.log('CHECK READY BRUH.');
+//         window.clearInterval(game.checkAudio);
+
+//     }
+// }
+
+
+// Animation loop here! Calls REMOVE WHEN ADDED TO POOL CLASS
+// function animate() {
+//     requestAnimFrame( animate );
+//     game.background.draw();
+// }
 
 // Creating the game object which will hold all the objects and data
 function Game() {
@@ -116,11 +207,11 @@ function Game() {
             this.background.init(0, 0);
             // Audio files
             // this.bgCanvas.addEventListener("mouseover", function( event ) {
-                this.backgroundAudio = new Audio("audio/ip.mp3");
-                this.backgroundAudio.loop = true;
-                this.backgroundAudio.volume = 0.2;
-                this.backgroundAudio.load();
-                this.backgroundAudio.play();
+                // this.backgroundAudio = new Audio("audio/ip.mp3");
+                // this.backgroundAudio.loop = true;
+                // this.backgroundAudio.volume = 0.2;
+                // this.backgroundAudio.load();
+                // this.backgroundAudio.play();
                 
             // })
             // Checking and looping audio with setInterval
@@ -139,19 +230,19 @@ function Game() {
     }
 };
 
-function startMusicXD() {
-    this.backgroundAudio = new Audio("audio/ip.mp3");
-    this.backgroundAudio.loop = true;
-    this.backgroundAudio.volume = 0.2;
-    this.backgroundAudio.load();
-    this.backgroundAudio.play();
-}
+// function startMusicXD() {
+//     this.backgroundAudio = new Audio("audio/ip.mp3");
+//     this.backgroundAudio.loop = true;
+//     this.backgroundAudio.volume = 0.2;
+//     this.backgroundAudio.load();
+//     this.backgroundAudio.play();
+// }
 
-// Creating a sound pool for sound effects
-// and game song.
-function SoundPool() {
+// // Creating a sound pool for sound effects
+// // and game song.
+// function SoundPool() {
 
-}
+// }
 
 
 
